b9afbfb4dd77cba186732454b552d94a285754c3
krisha-12569 <kdmorbi36@gmail.com>
2025-11-25 21:49:52 +0530

    Add files via upload

---
+++ p1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Krisha Dave");
+Console.WriteLine("SVG Road");
+Console.WriteLine("10909 54890");
+Console.WriteLine("Maharashtra");
