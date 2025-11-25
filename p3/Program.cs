392042643c938b9f916c5672ae828a20d9cd6ad4
krisha-12569 <kdmorbi36@gmail.com>
2025-11-25 21:43:13 +0530

    Add files via upload

---
+++ p3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter name and country name : ");
+
+string name = Console.ReadLine();
+string country_name = Console.ReadLine();
+
+Console.WriteLine("Hello " +name+ " from country " +country_name);
