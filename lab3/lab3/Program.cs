b2cc327e99032fcba5d35e57ede7beb5f4f74e6e
krisha-12569 <kdmorbi36@gmail.com>
2025-12-01 22:00:25 +0530

    Add files via upload

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,23 @@
+﻿using lab3;
+ 
+Student student1 = new Student();
+
+Console.WriteLine("Enter name : ");
+student1.Name = Console.ReadLine();
+Console.WriteLine("Enter Rollno : ");
+student1.rollno = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter marks : ");
+student1.marks = Convert.ToInt32(Console.ReadLine());
+
+student1.PrintData();
+
+Student student2 = new Student();
+
+Console.WriteLine("Enter name : ");
+student2.Name = Console.ReadLine();
+Console.WriteLine("Enter Rollno : ");
+student2.rollno = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter marks : ");
+student2.marks = Convert.ToInt32(Console.ReadLine());
+
+student2.PrintData();
---
+++ lab3/lab3/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    internal class Student
+    {
+        public string Name;
+        public int rollno;
+        public int marks;
+
+        public void PrintData()
+        {
+            Console.WriteLine(this.Name + " " +this.rollno +" " +this.marks);
+        }
+    }
+}
